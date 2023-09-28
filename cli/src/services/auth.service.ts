5461f8dd7ad9cab657d4d00734381c5eb5d2e44c
Fabio Sdringola <124870035+FabioSM46@users.noreply.github.com>
2023-09-28 11:15:06 +0200

    feat: able to log in

---
+++ cli/src/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { envClientSchema } from "@/clientEnvSchema";
+import axios from "axios";
+
+const API_URL = envClientSchema.REACT_APP_SERVER_URL;
+
+export const register = (username: string, email: string, password: string) => {
+  return axios.post(API_URL + "/auth/register", {
+    username,
+    email,
+    password,
+  });
+};
+
+export const login = (email: string, password: string) => {
+  return axios
+    .post(API_URL + "/auth/login", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+export const logout = () => {
+  localStorage.removeItem("user");
+};
+
+export const getCurrentUser = () => {
+  const userStr = localStorage.getItem("user");
+  if (userStr) return JSON.parse(userStr);
+
+  return null;
+};
