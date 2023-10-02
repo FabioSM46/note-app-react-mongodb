038ce8a37a73dedf6c8047d84da3f7de0d81cafe
Fabio Sdringola <124870035+FabioSM46@users.noreply.github.com>
2023-10-02 11:42:20 +0200

    feat: implement return of  logged in user data

---
+++ cli/src/context/auth.context.tsx
@@ -0,0 +1,90 @@
+import { useState, useEffect, createContext, PropsWithChildren } from 'react'
+import authService from '../services/auth.service'
+
+interface IAuth {
+  isLoggedIn: boolean
+  isLoading: boolean
+  user: null
+  storeToken: (token: string) => void
+  authenticateUser: () => void
+  logOutUser: () => void
+}
+
+const AuthContext = createContext({} as IAuth)
+
+function AuthProviderWrapper(props: PropsWithChildren) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [user, setUser] = useState(null)
+  const storeToken = (token: string): void => {
+    localStorage.setItem('authToken', token)
+  }
+
+  const authenticateUser = () => {
+    // Get the stored token from the localStorage
+    const storedToken = localStorage.getItem('authToken')
+
+    // If the token exists in the localStorage
+    if (storedToken) {
+      // We must send the JWT token in the request's "Authorization" Headers
+      authService
+        .verify()
+        .then((response) => {
+          // If the server verifies that the JWT token is valid
+          if (response.status !== 200) {
+            throw new Error('Invalid token')
+          }
+          const user = response.data
+
+          // Update state variables
+          setIsLoggedIn(true)
+          setIsLoading(false)
+          setUser(user)
+        })
+        .catch((error) => {
+          console.log(error)
+          setIsLoggedIn(false)
+          setIsLoading(false)
+          setUser(null)
+        })
+    } else {
+      // If the token is not available (or is removed)
+      setIsLoggedIn(false)
+      setIsLoading(false)
+      setUser(null)
+    }
+  }
+
+  const removeToken = () => {
+    // Upon logout, remove the token from the localStorage
+    localStorage.removeItem('authToken')
+  }
+
+  const logOutUser = () => {
+    // To log out the user, remove the token
+    removeToken()
+    // and update the state variables
+    authenticateUser()
+  }
+
+  useEffect(() => {
+    authenticateUser()
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        isLoading,
+        user,
+        storeToken,
+        authenticateUser,
+        logOutUser,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthProviderWrapper, AuthContext }
--- srv/src/router/users.ts
+++ srv/src/router/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
-import { isAuthenticated, isOwner } from "../middleware";
+import { isAuthenticated, isOwner } from "../middleware/index";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
