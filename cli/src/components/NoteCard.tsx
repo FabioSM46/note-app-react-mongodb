0f873e58170fa26e0ee0fdf5522e7da8f94153ec
Fabio Sdringola <124870035+FabioSM46@users.noreply.github.com>
2023-10-04 12:35:08 +0200

    feat: implement delete note

--- cli/src/components/NoteCard.tsx
+++ cli/src/components/NoteCard.tsx
@@ -6,14 +6,21 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import noteService from '@/services/note.service'
 import { FC } from 'react'
 
 interface NoteCardProps {
   title: string
   content?: string
+  noteId: string
+  onClick: () => void
 }
 
-const NoteCard: FC<NoteCardProps> = ({ title, content }) => {
+const NoteCard: FC<NoteCardProps> = ({ title, content, noteId, onClick }) => {
+  const delNote = async (id: string) => {
+    await noteService.deleteNote(id)
+    console.log('Note deleted')
+  }
   return (
     <Card className='w-[350px]'>
       <CardHeader>
@@ -21,7 +28,16 @@ const NoteCard: FC<NoteCardProps> = ({ title, content }) => {
       </CardHeader>
       <CardContent>{content}</CardContent>
       <CardFooter className='flex justify-between'>
-        <Button variant='outline'>Delete</Button>
+        <Button
+          variant='outline'
+          onClick={(e) => {
+            e.preventDefault()
+            delNote(noteId)
+            onClick()
+          }}
+        >
+          Delete
+        </Button>
         <Button>Edit</Button>
       </CardFooter>
     </Card>
--- srv/src/router/notes.ts
+++ srv/src/router/notes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getNotes, postNote } from "../controllers/notes";
+import { deleteNote, getNotes, postNote } from "../controllers/notes";
 import { isAuthenticated, isOwner } from "../middleware/index";
 
 export default (router: express.Router) => {
   router.post("/api/newnote", /* isAuthenticated, */ postNote);
   router.get("/api/notes/:id", /*  isAuthenticated, isOwner, */ getNotes);
+  router.delete("/api/notes/del/:id", /* isAuthenticated, isOwner, */ deleteNote);
 };
